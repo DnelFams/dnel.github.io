99ec431afba0556513a1a2c0fdbd4f1f7a579bc8
DnelFams <80157889+DnelFams@users.noreply.github.com>
2021-03-24 12:40:34 +0800

    Create tabmenu.php

---
+++ tabmenu.php
@@ -0,0 +1,11 @@
+<?php
+echo '<li><a href="https://dnelfams.github.io/mikrotik/autoblock-ipuser-mikrotik.html">Auto Block Ip User Jam Tertentu</a></li>
+      <li><a href="https://dnelfams.github.io/mikrotik/autoblock-traceroute.html">Auto Block Tracert or Traceroute</a></li>
+      <li><a href="#">JavaScript</a></li>
+      <li><a href="#">PHP</a></li>
+      <li><a href="#">Python</a></li>
+      <li><a href="#">jQuery</a></li>
+      <li><a href="#">SQL</a></li>
+      <li><a href="#">Bootstrap</a></li>
+      <li><a href="#">Node.js</a></li>';
+?>
